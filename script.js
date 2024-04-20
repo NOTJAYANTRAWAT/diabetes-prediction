f38fbe3246efa36c05e9ebb01d5fdd0e5944c5d3
Jayant Rawat <jayantrawat02@gmail.com>
2024-04-20 12:48:55 +0530

    final commit

--- script.js
+++ script.js
@@ -87,3 +87,66 @@ function openTab(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+function submitKidneyForm() {
+    // Get form elements
+    const form = document.getElementById('kidneyForm');
+    const formData = new FormData(form);
+
+    // Convert form data to JSON
+    const jsonData = {};
+    formData.forEach((value, key) => {
+        // Parse the value as an integer for certain fields
+        switch (key) {
+            case 'Age':
+                jsonData[key] = parseInt(value, 10);
+                break;
+            default:
+                jsonData[key] = value;
+        }
+    });
+
+    // Log that data is being sent for kidney prediction
+    console.log('Sending kidney prediction data:', jsonData);
+
+    // Send JSON data to the server for kidney prediction
+    fetch('https://jayant-major-kindey-zcyv.onrender.com/predict', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(jsonData),
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Log the server response to the console
+        console.log('Server Response:', data);
+    
+        // Extract the prediction value from the server response
+        const prediction = data.predictions[0];
+    
+        // Display a meaningful message based on the prediction value
+        const resultMessage = prediction == 1 ? 'Kidney failure predicted' : 'No kidney failure predicted.';
+    
+        console.log('Result Message:', resultMessage);
+    
+        // Set the result message in the result container
+        const resultContainer = document.getElementById('kidneyResult');
+        resultContainer.textContent = resultMessage;
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+// Update age value for kidney prediction
+document.getElementById('kidneyAge').addEventListener('input', function() {
+    document.getElementById('kidneyAgeValue').textContent = this.value;
+});
+
+// Update blood pressure value for kidney prediction
+document.getElementById('bloodPressure').addEventListener('input', function() {
+    document.getElementById('bloodPressureValue').textContent = this.value;
+});
+
+// Update blood glucose random value for kidney prediction
+document.getElementById('bloodGlucoseRandom').addEventListener('input', function() {
+    document.getElementById('bloodGlucoseRandomValue').textContent = this.value;
+});
